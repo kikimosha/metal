793e2e0fd42ccaa223c70264514b06519b3898f3
kikimosha <eszmvcu@mail.ru>
2014-09-02 00:39:30 +0300

    no message

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#waterwheel-carousel").waterwheelCarousel({
         horizon: 110,
-        horizonOffset: -50,
+        horizonOffset: 0,
         horizonOffsetMultiplier: .7,
         separation: 150,
         edgeFadeEnabled: true
